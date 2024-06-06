2a2c7e79e8191001a5fdfb7d95b947c1d2530b9b
Daniel <lucky19manrus@gmail.com>
2024-06-07 02:18:42 +0700

    fix remove console log

--- src/components/productCardContainer/ProductCardCell.jsx
+++ src/components/productCardContainer/ProductCardCell.jsx
@@ -6,10 +6,10 @@ const ProductCardCell = ({ sm, lg, children, justifyContent = 'center' }) => {
     const theme = useTheme();
     const isColumnLayout = useMediaQuery(theme.breakpoints.down('lg'));
 
-    const patchedJustifyContent = useMemo(() => {
-        console.log('isColumnLayout', isColumnLayout);
-        return isColumnLayout ? 'center' : justifyContent;
-    }, [isColumnLayout, justifyContent]);
+    const patchedJustifyContent = useMemo(
+        () => (isColumnLayout ? 'center' : justifyContent),
+        [isColumnLayout, justifyContent]
+    );
 
     return (
         <Grid
